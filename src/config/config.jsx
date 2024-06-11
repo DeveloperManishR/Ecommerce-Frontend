85759d200fbf502b63d21e4fba7a88132ae75db8
DevManishR <manishrawat0711@gmail.com>
2024-06-11 18:26:36 +0530

    Vite Env Correctly Implement

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -4,7 +4,7 @@ import { store } from '../Redux/store';
 
 export const withoutAuthAxios = () => {
   return axios.create({
-    baseURL: `${import.meta.env.REACT_APP_BASEURL}/api`
+    baseURL: `${import.meta.env.VITE_REACT_APP_BASEURL}/api`
   });
 };
 
@@ -12,7 +12,7 @@ export const withoutAuthAxios = () => {
 export const authAxios = () => {
   //let token = store.getState().auth.accessToken;
   return axios.create({
-    baseURL: `${import.meta.env.REACT_APP_BASEURL}/api`,
+    baseURL: `${import.meta.env.VITE_REACT_APP_BASEURL}/api`,
     headers: {
       'Authorization': `${token ? `${token}` : null}`,
     },
