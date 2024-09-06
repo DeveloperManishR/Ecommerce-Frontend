313aee994edfbb10cab5d35824af7c7f4be8e00a
DevManishR <manishrawat0711@gmail.com>
2024-09-06 17:19:52 +0530

    Product Page half implemnted

---
+++ src/Components/Admin/Model/ProductModel.jsx
@@ -0,0 +1,226 @@
+import React, { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+const ProductModel = ({ addProduct, setshowaddProduct,handleAddProduct }) => {
+  const [formData, setformData] = useState({
+    title: "",
+    category: "",
+    description: "",
+    price: "",
+    stock: "",
+    images: [],
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleclose = () => {
+    setshowaddProduct(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+    handleAddProduct(formData);
+  };
+
+  const handleUploadFile = (e) => {
+    //  console.log(e.target.files)
+    const files = e.target.files;
+
+    setformData((prev) => ({
+      ...prev,
+      images: [...formData?.images, ...Array.from(files)],
+    }));
+  };
+
+  console.log()
+
+  // const handleEditUpdate = () => {};
+
+  return (
+    <div
+      className="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <form
+          onSubmit={handleSubmit}
+          // onSubmit={productDetail?.id ? handleEditUpdate : handleSubmit}
+          className="flex min-h-full justify-center p-4 text-center items-center sm:p-0"
+        >
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg">
+            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 modal--popupservice">
+              <div className="pophead flex justify-between items-center mb-[10px]">
+                <h2 className="font-semibold text-[20px]">Add New Product</h2>
+
+                <div
+                  className="close cursor-pointer text-[20px]"
+                  onClick={handleclose}
+                >
+                  <IoCloseSharp />
+                </div>
+              </div>
+
+              <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                <div class="-mx-3 md:flex mb-6">
+                  <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      class="uppercase tracking-wide text-black text-xs font-bold mb-2"
+                      for="company"
+                    >
+                      Name
+                    </label>
+                    <input
+                      class="w-full bg-white text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      id="name"
+                      required
+                      name="title"
+                      onChange={handleChange}
+                      value={formData.title}
+                      type="text"
+                      placeholder=""
+                    />
+                    <div>
+                      {/*  <span class="text-red-500 text-xs italic">
+                        Please fill out this field.
+                      </span>
+                      */}
+                    </div>
+                  </div>
+                  <div class="md:w-1/2 px-3">
+                    <label
+                      class="uppercase tracking-wide text-black text-xs font-bold mb-2"
+                      for="category"
+                    >
+                      Category*
+                    </label>
+                    <input
+                      class="w-full bg-white text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      id="category"
+                      required
+                      type="text"
+                      name="category"
+                      onChange={handleChange}
+                      value={formData.category}
+                      placeholder=""
+                    />
+                  </div>
+                </div>
+                <div class="-mx-3 md:flex mb-6">
+                  <div class="md:w-full px-3">
+                    <label
+                      class="uppercase tracking-wide text-black text-xs font-bold mb-2"
+                      for="application-link"
+                    >
+                      Description*
+                    </label>
+                    <textarea
+                      class="w-full bg-white text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      id="application-link"
+                      type="text"
+                      name="description"
+                      value={formData.description}
+                      onChange={handleChange}
+                      // placeholder="http://...."
+                    />
+                  </div>
+                </div>
+                <div class="-mx-3 md:flex mb-2">
+                  <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      class="uppercase tracking-wide text-black text-xs font-bold mb-2"
+                      for="location"
+                    >
+                      Price*
+                    </label>
+                    <div>
+                      <input
+                        class="w-full bg-white border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded"
+                        name="price"
+                        onChange={handleChange}
+                        value={formData.price}
+                      />
+                    </div>
+                  </div>
+                  <div class="md:w-1/2 px-3">
+                    <label
+                      class="uppercase tracking-wide text-black text-xs font-bold mb-2"
+                      for="job-type"
+                    >
+                      Stock*
+                    </label>
+                    <div>
+                      <input
+                        class="w-full bg-white border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded"
+                        id="stock"
+                        name="stock"
+                        onChange={handleChange}
+                        value={formData.stock}
+                      />
+                    </div>
+                  </div>
+                  {/* 
+                  <div class="md:w-1/2 px-3">
+                    <label
+                      class="uppercase tracking-wide text-black text-xs font-bold mb-2"
+                      for="department"
+                    >
+                      Department*
+                    </label>
+                    <div>
+                      <input
+                        class="w-full bg-white border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded"
+                        id="location"
+                      />
+                    </div>
+                  </div>
+                  */}
+                </div>
+                <div class="-mx-3 md:flex mt-2">
+                  <div class="md:w-full px-3">
+                    <input
+                      type="file"
+                      required
+                      multiple
+                      onChange={handleUploadFile}
+                      // class="md:w-full bg-gray-900 text-white font-bold py-2 px-4 border-b-4 hover:border-b-2 border-gray-500 hover:border-gray-100 rounded-full"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 gap-[10px] justify-end">
+              <button
+                type="submit"
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                // onClick={() => handleaddCategory(CategoryName)}
+              >
+                Add Product
+              </button>
+
+              <button
+                type="button"
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                // onClick={handleclose}
+                // onClick={() => setshowAddCategory(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ProductModel;
--- src/Components/Admin/Products.jsx
+++ src/Components/Admin/Products.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { withoutAuthAxios } from "../../config/config";
+import { authAxios, withoutAuthAxios } from "../../config/config";
 import {
   Bars3Icon,
   BellIcon,
   EllipsisVerticalIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import ProductModel from "./Model/ProductModel";
+import { toast } from "react-toastify";
 const Products = () => {
   const [products, setproducts] = useState([]);
+
+  const [model, setmodel] = useState({
+    show: false,
+  });
 
   const fetchAllproducts = async () => {
     await withoutAuthAxios()
@@ -22,7 +30,51 @@ const Products = () => {
       });
   };
 
-  console.log(products);
+  const handleAddProduct = async (data) => {
+    const formData = new FormData();
+    data?.images?.forEach((file) => {
+      formData.append("images", file);
+    });
+
+    formData.append("title", data.title);
+    formData.append("category", data.category);
+    formData.append("description", data.description);
+    formData.append("price", data.price);
+    formData.append("stock", data.stock);
+
+    await authAxios()
+      .post(`/product/create-product`, formData)
+      .then((response) => {
+        const resData = response.data;
+        toast.success(resData.message);
+        fetchAllproducts();
+        setmodel((prev) => ({
+          ...prev,
+          show: false,
+        }));
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+
+  const handleDeleteProduct = async (id) => {
+    await authAxios()
+      .delete(`/product/delete-product/${id}`)
+      .then((response) => {
+        const resData = response.data;
+        toast.success(resData.message);
+        fetchAllproducts()
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+
+  const [currentDropdown, setcurrentDropdown] = useState([]);
+  const handleEditProduct = () => {};
+
+  console.log("curr", currentDropdown);
 
   useEffect(() => {
     fetchAllproducts();
@@ -31,6 +83,20 @@ const Products = () => {
   return (
     <div className="flex-1 p-6">
       <h2 className="text-2xl font-semibold mb-6">Products</h2>
+
+      <div className="flex gap-[5px] gap-y-[10px] flex-wrap w-full justify-end">
+        <button className="flex items-center max-w-[140px] border border-white w-full justify-center bg-white text-textcolor py-[8px] px-[10px] h-[39px] rounded-[5px] duration-[0.3s] hover:bg-transparent hover:border-textcolor hover:text-textcolor h-[45px] font-[600] min-h">
+          Total Products: {products?.length}
+        </button>
+
+        <button
+          onClick={() => setmodel((prev) => ({ ...prev, show: true }))}
+          className=" bg-black flex items-center max-w-[160px] border border-buttonbg w-full justify-center bg-buttonbg text-white py-[8px] px-[15px] h-[39px] rounded-[5px] duration-[0.3s]  h-[45px] font-[600] min-h"
+        >
+          Add Product
+        </button>
+      </div>
+
       <div className="bg-white shadow overflow-hidden sm:rounded-lg">
         <table className="min-w-full table-auto">
           <thead>
@@ -68,25 +134,70 @@ const Products = () => {
                   <td className="px-6 py-4 whitespace-nowrap">
                     {item?.rating}
                   </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10.5 6a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm0 6a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm0 6a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                  <td
+                    onClick={() => setcurrentDropdown(item)}
+                    className="px-6 py-4 whitespace-nowrap"
+                  >
+                    <Menu as="div" className="relative inline-block text-left">
+                      <div>
+                        <MenuButton className="">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                            />
+                          </svg>
+                        </MenuButton>
+                      </div>
+
+                      <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                      >
+                        <div className="py-1">
+                          <MenuItem>
+                            <li className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900">
+                              View
+                            </li>
+                          </MenuItem>
+                          <MenuItem>
+                            <li className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900">
+                              Edit
+                            </li>
+                          </MenuItem>
+                          <MenuItem>
+                            <li
+                              onClick={() => handleDeleteProduct(item._id)}
+                              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                            >
+                              Delete
+                            </li>
+                          </MenuItem>
+                        </div>
+                      </MenuItems>
+                    </Menu>
                   </td>
                 </tr>
               ))}
           </tbody>
         </table>
       </div>
+
+      {model.show && (
+        <ProductModel
+          setmodel={setmodel}
+          model={model}
+          handleAddProduct={handleAddProduct}
+        />
+      )}
     </div>
   );
 };
